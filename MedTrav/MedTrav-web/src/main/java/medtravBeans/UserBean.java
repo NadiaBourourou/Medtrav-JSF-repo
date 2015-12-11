816f068c7ab91303eb0f018b36fb6901db6d7d10
Toshiba <Toshiba@Toshiba-PC>
2015-12-11 10:37:17 +0100

    Gestion docteur complète : ajout + edit + vérification des champs

--- MedTrav/MedTrav-web/src/main/java/medtravBeans/UserBean.java
+++ MedTrav/MedTrav-web/src/main/java/medtravBeans/UserBean.java
@@ -9,20 +9,24 @@ import javax.faces.bean.SessionScoped;
 import services.interfaces.UserServicesLocal;
 import entities.Doctor;
 import entities.Patient;
+import entities.RoleType;
 
 @ManagedBean
 @SessionScoped
 public class UserBean {
 
 	private Patient patient = new Patient();
+	private Doctor doctor = new Doctor();
 	private String patientSignature;
-
 	private List<Doctor> doctors;
+	
+	private Boolean diplayAddDoctorForm=false;
 
 	@EJB
 	private UserServicesLocal userServices;
 
 	public String doAddPatient() {
+		patient.setRole(RoleType.PATIENT);
 		if (userServices.addPatient(patient))
 			return ""; // user registered
 
@@ -30,11 +34,28 @@ public class UserBean {
 			return ""; // a problem occurred during user registration
 	}
 	
+	public void displayForm()
+	{
+		diplayAddDoctorForm = true;
+		}
+	
+	public String doAddDoctor(){
+	doctor.setRole(RoleType.DOCTOR);
+	doctor.setLogin(doctor.getFirstName()+"."+doctor.getLastName());
+	doctor.setPassword("pwd"+doctor.getFirstName());
+	userServices.addDocor(doctor);
+	
+	diplayAddDoctorForm=false;
+	return "admin_listOfDoctors";
+	}
+	
 	public String doUpdateDoctor(Doctor doctor){
 	
 		userServices.updateDoctor(doctor);
 		return"";
 	}
+	
+
 
 	public String doBookDoctor(Doctor doctor) {
 		userServices.chooseDoctor(doctor, 1);
@@ -65,5 +86,23 @@ public class UserBean {
 	public void setDoctors(List<Doctor> doctors) {
 		this.doctors = doctors;
 	}
+
+	public Boolean getDiplayAddDoctorForm() {
+		return diplayAddDoctorForm;
+	}
+
+	public void setDiplayAddDoctorForm(Boolean diplayAddDoctorForm) {
+		this.diplayAddDoctorForm = diplayAddDoctorForm;
+	}
+
+	public Doctor getDoctor() {
+		return doctor;
+	}
+
+	public void setDoctor(Doctor doctor) {
+		this.doctor = doctor;
+	}
+	
+	
 
 }
